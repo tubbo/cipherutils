fdb9eb59dc9a9caa63d62f099775e3fbccdf4657
Tom Scott <tscott@weblinc.com>
2020-01-19 15:59:33 -0500

    Add hex to byteme

--- byteme/go.mod
+++ byteme/go.mod
@@ -2,4 +2,9 @@ module github.com/tubbo/cipherutils/byteme
 
 go 1.13
 
-require github.com/yuya-takeyama/argf v0.0.0-20150217044922-3ff699b4b958
+require (
+	github.com/tubbo/cipherutils/dictionary v0.0.0-00010101000000-000000000000
+	github.com/yuya-takeyama/argf v0.0.0-20150217044922-3ff699b4b958
+)
+
+replace github.com/tubbo/cipherutils/dictionary => ../dictionary
--- byteme/main.go
+++ byteme/main.go
@@ -2,35 +2,51 @@ package main
 
 import (
 	"encoding/hex"
-	"flag"
 	"fmt"
+	"github.com/tubbo/cipherutils/dictionary"
 	"github.com/yuya-takeyama/argf"
 	"io/ioutil"
+	"strings"
 )
 
-var encoding = flag.Bool("h", false, "Decode from hexadecimal")
-
-func hexadecimal(input string) string {
+func hexadecimal(input string) {
 	var output []byte
 
 	bytes := []byte(input)
 
 	hex.Decode(output, bytes)
 
-	return string(output)
+	results := string(output)
+	words := strings.Split(results, " ")
+	count := 0
+
+	for _, word := range words {
+		count += dictionary.Lookup(word)
+	}
+
+	if count > 0 {
+		fmt.Println(results)
+	}
 }
 
-func binary(input string) string {
-	return input
+func binary(input string) {
+	results := string(input)
+	words := strings.Split(results, " ")
+	count := 0
+
+	for _, word := range words {
+		count += dictionary.Lookup(word)
+	}
+
+	if count > 0 {
+		fmt.Println(results)
+	}
 }
 
 // Decode a string from binary to text
-func Decode(input string, isHex bool) string {
-	if isHex {
-		return hexadecimal(input)
-	}
-
-	return binary(input)
+func Decode(input string) {
+	go hexadecimal(input)
+	go binary(input)
 }
 
 func main() {
@@ -43,7 +59,6 @@ func main() {
 		panic(err)
 	}
 	input := string(buf)
-	result := Decode(input, true)
 
-	fmt.Println(result)
+	Decode(input)
 }
